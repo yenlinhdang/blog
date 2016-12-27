81f6b809ef93b228ab6a3c1353185e053edbe55a
NguyenSi <nguyensi43@gmail.com>
2016-12-27 13:50:12 +0700

    translate to slim

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+before_action :authenticate_admin!, except: [:index, :show]
+
+   def index
+    @articles = Article.all
+  end
+ 
+  def show
+    @article = Article.find(params[:id])
+  end
+ 
+  def new
+    @article = current_admin.articles.new
+  end
+ 
+  def edit
+    @article = Article.find(params[:id])
+  end
+ 
+  def create
+    @article =current_admin.articles.new(article_params)
+ 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+ 
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
+ 
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  devise_for :admins
+  mount Ckeditor::Engine => '/ckeditor'
+root to: "articles#index"
+resources :articles
+resources :articles do
+ resources :comments
+end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
